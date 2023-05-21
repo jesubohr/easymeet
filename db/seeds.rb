f91746d838c338f76fc0a8b4125a0ec17817fb3e
Jesus Borrero <jesufer2018@hotmail.com>
2023-05-21 02:46:46 -0500

    Create test data for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+BookingType.destroy_all
+
+user = User.create!(
+  booking_link: "jameswood",
+  name: "James Wood",
+  email: "james@gmail.com",
+  password: "password",
+  password_confirmation: "password",
+)
+
+bookin_type1 = BookingType.create!(
+  user: user,
+  name: "15 Minute Meeting",
+  location: "Zoom",
+  description: "A 15 minute meeting with James Wood",
+  color: "#2ecc71",
+  duration: 15,
+  payment_required: false,
+  price: nil,
+)
+
+bookin_type2 = BookingType.create!(
+  user: user,
+  name: "30 Minute Meeting",
+  location: "Zoom",
+  description: "A 30 minute meeting with James Wood",
+  color: "#3498db",
+  duration: 30,
+  payment_required: true,
+  price: 5,
+)
+
+bookin_type3 = BookingType.create!(
+  user: user,
+  name: "60 Minute Meeting",
+  location: "Zoom",
+  description: "A 1 hour meeting with James Wood",
+  color: "#e4a241",
+  duration: 60,
+  payment_required: true,
+  price: 10,
+)
+
+puts "Created #{User.count} users"
+puts "Created #{BookingType.count} booking types"
