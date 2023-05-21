b8d89bbd8abcd5e0d5dc8f24c7a7a5ee0c9f6234
Jesus Borrero <jesufer2018@hotmail.com>
2023-05-21 00:03:37 -0500

    Routes for dashboard, bookings, booking_types and specific booking_link

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,17 @@ Rails.application.routes.draw do
       mount Sidekiq::Web => '/sidekiq'
     end
 
-
   devise_for :users
+
+  authenticated :user do
+    root to: 'home#dashboard', as: :authenticated_root
+  end
+
+  resources :booking_types
+  resources :bookings
+
+  get ':booking_link', to: 'users#show', as: :user
+  
   root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
