562ad70f1e13ce4009bd4b101b5c3236103dc131
Jesus Borrero <jesufer2018@hotmail.com>
2023-05-21 02:31:32 -0500

    Dashboard panel for displaying bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   has_person_name
   belongs_to :booking_type
-  belongs_to :user
   has_rich_text :notes
+
+  enum status: { pending: 0, approved: 1, rejected: 2 }
 end
